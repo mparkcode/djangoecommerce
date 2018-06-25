44fbe31cd51401b48a9d0e2bb8758c5ee2b02b7e
Michael Park <mparkcode@gmail.com>
2018-06-25 15:51:59 +0000

    Added products app and displayed front page including base.html

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from products import urls as products_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(products_urls)),
 ]
